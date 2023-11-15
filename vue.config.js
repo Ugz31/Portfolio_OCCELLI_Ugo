3826fb2031c6150b3dcfe070c6aa56026ceba36f
Ugz31 <occelli.ugo@gmail.com>
2023-11-15 12:41:07 +0100

    fix: dist

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Portofolio_OCCELLI_Ugo/' // Remplacez 'mon-repo-name' par le nom de votre repository GitHub.
-    : '/'
-}
